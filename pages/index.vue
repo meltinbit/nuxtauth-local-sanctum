ae2ef7bca18267ed614faa3ce8e79d8ebd3d1075
Fabio Mangolini <fabio@meltinbit.com>
2020-06-15 15:01:49 +0200

    real initial working commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <div v-if="$auth.loggedIn">
+      Welcome {{  $auth.user.name }}
+      <button @click="$auth.logout()">Logout</button>
+    </div>
+    <div v-else>
+      Devi prima accedere <nuxt-link to="/login">Login</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+
+}
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <nuxt-link to="/">home</nuxt-link>
+    <input v-model="form.email">
+    <input v-model="form.password">
+    <button @click="login">LOGIN</button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data() {
+    return {
+      form: {
+        email: 'fabio@fabiomangolini.net',
+        password: 'zxasqw12'
+      }
+    }
+  },
+  methods: {
+    login() {
+      this.$auth.loginWith('local', {
+        data: this.form
+      })
+      .then( user => {
+        this.$auth.setUser(user.data)
+      })
+      .catch( err => {
+        console.error(err)
+      })
+    }
+  }
+}
+</script>
